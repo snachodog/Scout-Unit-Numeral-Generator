ec00d8c188013d8b1a3de5e91a1db784e7a29e52
Steve Dogiakos <dogiakos@gmail.com>
2020-05-27 22:06:13 -0600

    Adding images and first draft

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Scout Unit Numeral Generator
  A tool for creating images of Troop, Pack, Crew Numerals
+
+This tool that creates "unit numerals" for Scout Troops.
+
+The flow is the user inputs their unit number, (228 or 51) and the tool will put the associated images (five.png, and one.png) for each numeral next to each other and output a file (five_one.png).
---
+++ numeral-generator.py
@@ -0,0 +1,15 @@
+from PIL import Image
+# get the numeral images - add pack numeals later
+one = Image.open("one.jpg")
+two = Image.open("two.jpg")
+three = Image.open("three.jpg")
+four = Image.open("four.jpg")
+five = Image.open("five.jpg")
+six = Image.open("six.jpg")
+seven = Image.open("seven.jpg")
+eight = Image.open("eight.jpg")
+nine = Image.open("nine.jpg")
+zero = Image.open("zero.jpg")
+# get the unit number - add pack as an option later
+unitno = input("What is your Troop number?")
+# parse the troop number
